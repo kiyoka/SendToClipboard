825bebd55184940166e25ac8c6b00cc2363b88da
Kiyoka Nishiyama <kiyoka@sumibi.org>
2013-08-04 10:41:33 +0900

    first version

---
+++ SendToClipboard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendToClipboard
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    System.Console.WriteLine("SendToClipboard \"string\"");
+                    break;
+                default:
+                    SendToClipboard(args[0]);
+                    break;
+            }
+            return 0;
+        }
+
+        static void SendToClipboard(string filepath)
+        {
+            //System.Console.WriteLine(filepath);
+            try
+            {
+                Clipboard.SetText(filepath);
+            }
+            catch (ArgumentNullException e)
+            {
+                System.IO.TextWriter errorWriter = Console.Error;
+                errorWriter.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
