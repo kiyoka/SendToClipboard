6ec0943c540aeaddb17f8f212d9e23aaebfed239
Kiyoka Nishiyama <kiyoka@sumibi.org>
2013-08-10 22:49:56 +0900

    Support two feature "FullPath To Clipboard" and "FilePath To Clipboard".

--- FileName/Program.cs
+++ FileName/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace SendToClipboard
+namespace FileNameToClipboard
 {
     class Program
     {
@@ -15,7 +15,7 @@ namespace SendToClipboard
             switch (args.Length)
             {
                 case 0:
-                    System.Console.WriteLine("SendToClipboard \"string\"");
+                    System.Console.WriteLine("FileNameToClipboard \"string1\" \"string2\" ...");
                     break;
                 default:
                     SendToClipboard(args);
@@ -24,12 +24,20 @@ namespace SendToClipboard
             return 0;
         }
 
+        static string[] getFileNames(string[] filePathArray)
+        {
+            List<string> lst = new List<string>();
+            foreach( string filepath in filePathArray ) {
+                lst.Add(System.IO.Path.GetFileName(filepath));
+            }
+            return lst.ToArray();
+        }
+
         static void SendToClipboard(string[] filePathArray)
         {
             string text = "";
-            foreach( string filepath in filePathArray ) {
-                text += filepath + "\n";
-            }
+            text = string.Join("\n", getFileNames(filePathArray));
+      
             //System.Console.WriteLine(text);
             try
             {
---
+++ FullPath/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FullPathToClipboard
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    System.Console.WriteLine("FullPathToClipboard \"string1\" \"string2\" ...");
+                    break;
+                default:
+                    SendToClipboard(args);
+                    break;
+            }
+            return 0;
+        }
+
+        static void SendToClipboard(string[] filePathArray)
+        {
+            string text = "";
+            text = string.Join("\n", filePathArray);
+
+            //System.Console.WriteLine(text);
+            try
+            {
+                Clipboard.SetText(text);
+            }
+            catch (ArgumentNullException e)
+            {
+                System.IO.TextWriter errorWriter = Console.Error;
+                errorWriter.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
